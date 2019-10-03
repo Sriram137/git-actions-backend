9fdea06ea84d101fe6034522b8fb4046a5d81fd0
Balram Khichar <beingbalram@gmail.com>
2019-10-03 18:12:17 +0530

    Logs added Logs added

--- src/main.ts
+++ src/main.ts
@@ -17,55 +17,29 @@ async function run() {
       return;
     }
 
-    const prNumber = getPrNumber();
-    if (!prNumber) {
-      core.debug('Could not get pull request number from context');
-      return;
-    }
-
     const teamName = `${_.capitalize(reviewTeamSlug)}-Team`;
     const client = new github.GitHub(token);
-    if (github.context.eventName === 'pull_request_review') {
-      await reRunFrontendCheck(client);
+
+    console.log(`EVENT: ${github.context.eventName}`);
+    if (github.context.eventName === 'push') {
+      await handlePushEvent(client, teamName, coreReviewers, additionalReviewers, reviewTeamSlug);
       return;
     }
 
-    console.log(`PROCESSING: Fetching changed files for pr #${prNumber}`);
-    const changedFiles: string[] = await getChangedFiles(client, prNumber);
-    const hasChanges: boolean = hasStyleChanges(changedFiles);
-
-    if (hasChanges) {
-      console.log(`STATUS: Checking ${teamName} approval status`);
-      const approvedReviewers: string[] = await getApprovedReviews(client, prNumber);
-      let approvalNeeded: boolean = true;
-      if (approvedReviewers.length) {
-        console.log(`Pull request is approved by ${approvedReviewers.join(', ')}`);
-        const reviewTeamMembers: string[] = await getReviewers(client, org, reviewTeamSlug);
-        if (_.isEmpty(reviewTeamMembers)) {
-          core.setFailed(`${teamName} has no members`);
-          return;
-        } else if (_.intersection(approvedReviewers, [...reviewTeamMembers, ..._.split(additionalReviewers, '')]).length > 0) {
-          approvalNeeded = false;
-        }
-      }
-
-      if (approvalNeeded) {
-        await addReviewers(client, prNumber, coreReviewers);
-        core.setFailed(`${teamName} approval needed`);
-      } else {
-        console.log(`${teamName} approved changes.`);
-      }
-    } else {
-      console.log(`No approval needed from ${teamName}`);
+    if (github.context.eventName === 'pull_request_review') {
+      await reTriggerFrontendCheck(client);
+      return;
     }
+
+    console.log('ERROR: Event not handled');
   } catch (error) {
     core.setFailed(error.message);
   }
 }
 
-async function getReviewers(client: github.GitHub, org: string, reviewTeamSlug: string): Promise<string[]> {
+async function getReviewers(client: github.GitHub, reviewTeamSlug: string): Promise<string[]> {
   const team = await client.teams.getByName({
-    org,
+    org: github.context.repo.owner,
     team_slug: reviewTeamSlug,
   });
   if (!team) {
@@ -76,31 +50,6 @@ async function getReviewers(client: github.GitHub, org: string, reviewTeamSlug: 
     team_id: teamId,
   });
   return _.map(members.data, 'login');
-}
-
-async function getApprovedReviews(client: github.GitHub, prNumber: number): Promise<string[]> {
-  const listReviewRequests = await client.pulls.listReviews({
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo,
-    pull_number: prNumber,
-  });
-  return _(listReviewRequests.data)
-    .filter({ state: 'APPROVED' })
-    .map('user.login')
-    .uniq()
-    .value();
-}
-
-function getPrNumber(): number | undefined {
-  const payload = github.context.payload;
-  let pullRequest = payload.pull_request;
-  if (!pullRequest && payload.action === 'rerequested') {
-    pullRequest = payload.check_suite.pull_requests[0];
-  }
-  if (!pullRequest) {
-    return undefined;
-  }
-  return pullRequest.number;
 }
 
 async function getChangedFiles(
@@ -116,7 +65,7 @@ async function getChangedFiles(
   return changedFiles;
 }
 
-function hasStyleChanges(changedFiles: string[]): boolean {
+function hasReviewableChanges(changedFiles: string[]): boolean {
   if (_.isEmpty(changedFiles)) {
     return false
   }
@@ -128,29 +77,6 @@ function hasStyleChanges(changedFiles: string[]): boolean {
   // ));
 }
 
-async function reRunFrontendCheck(client: github.GitHub) {
-  console.log('Finding Frontend Review Check');
-  const checkListResponse = await client.checks.listForRef({
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo,
-    ref: github.context.sha,
-    check_name: 'FrontendReviewCheck',
-  });
-
-  if (!_.isEmpty(checkListResponse.data.check_runs)) {
-    console.log('Re-triggering Frontend Review Check');
-    checkListResponse.data.check_runs[0]
-    await client.checks.rerequestSuite({
-      owner: github.context.repo.owner,
-      repo: github.context.repo.repo,
-      check_suite_id: checkListResponse.data.check_runs[0].check_suite.id
-    })
-  } else {
-    console.log('No Check Found');
-    console.log(checkListResponse.data);
-  }
-}
-
 async function addReviewers(client: github.GitHub, prNumber: number, coreReviewers: string) {
   const pullRequestResponse = await client.pulls.get({
     owner: github.context.repo.owner,
@@ -160,13 +86,109 @@ async function addReviewers(client: github.GitHub, prNumber: number, coreReviewe
 
   console.log(`PR author: ${pullRequestResponse.data.user.login}`);
   const reviewers = _.filter(_.split(coreReviewers, ','), reviewer => reviewer !== pullRequestResponse.data.user.login);
-  console.log(`Requestung review from: ${_.join(reviewers, ', ')}`);
+  console.log(`Requesting review from: ${_.join(reviewers, ', ')}`);
   client.pulls.createReviewRequest({
     owner: github.context.repo.owner,
     repo: github.context.repo.repo,
     pull_number: prNumber,
     reviewers: reviewers,
   })
+}
+
+// Re trigger event
+async function reTriggerFrontendCheck(client: github.GitHub) {
+  console.log('LOG: Finding Frontend Review Check');
+  const pullRequest = github.context.payload.pull_request;
+  if (pullRequest) {
+    const headSHA = await getPullRequestHeadSHA(client, pullRequest.number);
+    console.log(`LOG: Finding checks for PR: ${headSHA}`)
+    const checkListResponse = await client.checks.listForRef({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      ref: headSHA,
+    });
+    console.log(`LOG: Found ${checkListResponse.data.check_runs.length} check runs.`)
+    const reviewCheck = _.find(checkListResponse.data.check_runs, { name: 'FrontendReviewStatus' });
+    if (reviewCheck) {
+      console.log(`LOG: Re-triggering Review Check ${reviewCheck.name}`);
+      await client.checks.rerequestSuite({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        check_suite_id: reviewCheck.check_suite.id
+      });
+    } else {
+      console.log(checkListResponse.data.check_runs);
+      core.setFailed('ERROR: No matching check found.');
+    }
+  } else {
+    core.setFailed('ERROR: Pull request not found.');
+  }
+}
+async function getPullRequestHeadSHA(client: github.GitHub, pull_number: number) {
+  const pullRequestResponse = await client.pulls.get({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    pull_number
+  });
+  return pullRequestResponse.data.head.sha;
+}
+// PUSH EVENT HANDLING
+async function handlePushEvent(client: github.GitHub, teamName: string, coreReviewers: string, additionalReviewers: string, reviewTeamSlug: string) {
+  const pullRequest = await getPullRequestForSHA(client, github.context.sha);
+  if (!pullRequest) {
+    console.log('ERROR: Pull request not found.');
+    return;
+  }
+  console.log(`PROCESSING: Fetching changed files for PR:#${pullRequest.number}`);
+  const changedFiles: string[] = await getChangedFiles(client, pullRequest.number);
+  const hasChanges: boolean = hasReviewableChanges(changedFiles);
+  if (hasChanges) {
+    const isApproved = await checkApprovalForSHA(client, pullRequest.number, github.context.sha, additionalReviewers, reviewTeamSlug);
+    if (isApproved) {
+      console.log(`SUCCESS: ${teamName} approved changes.`);
+    } else {
+      await addReviewers(client, pullRequest.number, coreReviewers);
+      core.setFailed(`ERROR: ${teamName} approval needed`);
+    }
+  } else {
+    console.log(`SUCCESS: No approval needed from ${teamName}.}`);
+  }
+}
+async function getPullRequestForSHA(client: github.GitHub, commit_sha: string) {
+  const pullRequests = await listPullRequestsAssociatedWithCommit(client, github.context.sha);
+  const openPullRequests = _.filter(pullRequests, { state: 'open' });
+  if (_.size(openPullRequests) > 1) {
+    console.log(`WARNING: Multiple pull requests found for SHA: ${github.context.sha}.`)
+  }
+  return openPullRequests[0];
+}
+async function listPullRequestsAssociatedWithCommit(client: github.GitHub, commit_sha: string) {
+  const apiResponse = await client.repos.listPullRequestsAssociatedWithCommit({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    commit_sha
+  });
+  return apiResponse.data;
+}
+async function checkApprovalForSHA(client: github.GitHub, pull_number: number, commit_sha: string, additionalReviewers: string, reviewTeamSlug: string): Promise<boolean> {
+  console.log('STATUS: Checking approval status');
+  const approvedLogins = await getApprovedReviewsForSHA(client, pull_number, commit_sha);
+  const reviewTeamMembers: string[] = await getReviewers(client, reviewTeamSlug);
+  if (_.intersection(approvedLogins, [...reviewTeamMembers, ..._.split(additionalReviewers, '')]).length > 0) {
+    return true;
+  }
+  return false;
+}
+async function getApprovedReviewsForSHA(client: github.GitHub, pull_number: number, commit_sha: string) {
+  const apiResponse = await client.pulls.listReviews({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    pull_number
+  });
+  return _(apiResponse.data)
+    .filter({ commit_id: commit_sha, state: 'APPROVED' })
+    .map('user.login')
+    .value();
 }
 
 run();
